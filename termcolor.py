016af07c3496143ba253d4c7ef2f96f8d44c56bf
ehuber <ehuber@rollerbooger.(none)>
2012-03-07 16:06:17 -0800

    the same, except that now it silently fixes nested color/attribute sequences I think

--- termcolor.py
+++ termcolor.py
@@ -25,6 +25,7 @@
 
 from __future__ import print_function
 import os
+import re
 
 
 __ALL__ = [ 'colored', 'cprint' ]
@@ -47,6 +48,7 @@ ATTRIBUTES = dict(
         )
 del ATTRIBUTES['']
 
+ATTRIBUTES_RE = '\033\[(?:%s)m' % '|'.join(['%d' % v for v in ATTRIBUTES.values()])
 
 HIGHLIGHTS = dict(
         list(zip([
@@ -63,6 +65,7 @@ HIGHLIGHTS = dict(
             ))
         )
 
+HIGHLIGHTS_RE = '\033\[(?:%s)m' % '|'.join(['%d' % v for v in HIGHLIGHTS.values()])
 
 COLORS = dict(
         list(zip([
@@ -79,12 +82,14 @@ COLORS = dict(
             ))
         )
 
+COLORS_RE = '\033\[(?:%s)m' % '|'.join(['%d' % v for v in COLORS.values()])
 
 RESET = '\033[0m'
+RESET_RE = '\033\[0m'
 
 
 def colored(text, color=None, on_color=None, attrs=None):
-    """Colorize text.
+    """Colorize text, while stripping nested ANSI color sequences.
 
     Available text colors:
         red, green, yellow, blue, magenta, cyan, white.
@@ -102,17 +107,18 @@ def colored(text, color=None, on_color=None, attrs=None):
     if os.getenv('ANSI_COLORS_DISABLED') is None:
         fmt_str = '\033[%dm%s'
         if color is not None:
+            text = re.sub(COLORS_RE + '(.*?)' + RESET_RE, r'\1', text)
             text = fmt_str % (COLORS[color], text)
-
         if on_color is not None:
+            text = re.sub(HIGHLIGHTS_RE + '(.*?)' + RESET_RE, r'\1', text)
             text = fmt_str % (HIGHLIGHTS[on_color], text)
-
         if attrs is not None:
+            text = re.sub(ATTRIBUTES_RE + '(.*?)' + RESET_RE, r'\1', text)
             for attr in attrs:
                 text = fmt_str % (ATTRIBUTES[attr], text)
-
-        text += RESET
-    return text
+        return text + RESET
+    else:
+        return text
 
 
 def cprint(text, color=None, on_color=None, attrs=None, **kwargs):
